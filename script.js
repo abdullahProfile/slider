f76ecd53685edc9829970b5c31142e6fbe338d25
abdullahProfile <abdullahdew88@gmail.com>
2025-08-11 18:51:51 +0500

    slider

---
+++ script.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 'auto',
+  spaceBetween: 20,
+  centeredSlides: true,
+  loop: false,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  }
+});
